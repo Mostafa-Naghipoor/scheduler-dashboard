972481bc87301b204621057008736c10356f3ee0
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-01-21 14:51:26 -0800

    Update after vm tests

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -1,87 +1,206 @@
-import { flow } from "lodash/fp";
+exports[`selectors setInterview should return state with new interview when interview is not null 1`] = `
+Object {
+  "appointments": Object {
+    "1": Object {
+      "id": 1,
+      "interview": null,
+      "time": "12pm",
+    },
+    "2": Object {
+      "id": 2,
+      "interview": Object {
+        "interviewer": 1,
+        "student": "Archie Cohen",
+      },
+      "time": "1pm",
+    },
+    "3": Object {
+      "id": 3,
+      "interview": null,
+      "time": "12pm",
+    },
+    "4": Object {
+      "id": 4,
+      "interview": null,
+      "time": "1pm",
+    },
+  },
+  "days": Array [
+    Object {
+      "appointments": Array [
+        1,
+        2,
+      ],
+      "id": 1,
+      "interviewers": Array [
+        1,
+        2,
+      ],
+      "name": "Monday",
+      "spots": 1,
+    },
+    Object {
+      "appointments": Array [
+        3,
+        4,
+      ],
+      "id": 2,
+      "interviewers": Array [
+        1,
+        2,
+      ],
+      "name": "Tuesday",
+      "spots": 2,
+    },
+  ],
+  "interviewers": Object {
+    "1": Object {
+      "avatar": "https://i.imgur.com/LpaY82x.png",
+      "id": 1,
+      "name": "Sylvia Palmer",
+    },
+    "2": Object {
+      "avatar": "https://i.imgur.com/Nmx0Qxo.png",
+      "id": 2,
+      "name": "Tori Malcolm",
+    },
+  },
+}
+`;
 
-import {
-  getTotalInterviews,
-  getLeastPopularTimeSlot,
-  getInterviewsPerDay,
-  getMostPopularDay
-} from "./selectors";
+exports[`selectors setInterview should return state with no interview when interview is not null 1`] = `
+Object {
+  "appointments": Object {
+    "1": Object {
+      "id": 1,
+      "interview": null,
+      "time": "12pm",
+    },
+    "2": Object {
+      "id": 2,
+      "interview": null,
+      "time": "1pm",
+    },
+    "3": Object {
+      "id": 3,
+      "interview": null,
+      "time": "12pm",
+    },
+    "4": Object {
+      "id": 4,
+      "interview": null,
+      "time": "1pm",
+    },
+  },
+  "days": Array [
+    Object {
+      "appointments": Array [
+        1,
+        2,
+      ],
+      "id": 1,
+      "interviewers": Array [
+        1,
+        2,
+      ],
+      "name": "Monday",
+      "spots": 2,
+    },
+    Object {
+      "appointments": Array [
+        3,
+        4,
+      ],
+      "id": 2,
+      "interviewers": Array [
+        1,
+        2,
+      ],
+      "name": "Tuesday",
+      "spots": 2,
+    },
+  ],
+  "interviewers": Object {
+    "1": Object {
+      "avatar": "https://i.imgur.com/LpaY82x.png",
+      "id": 1,
+      "name": "Sylvia Palmer",
+    },
+    "2": Object {
+      "avatar": "https://i.imgur.com/Nmx0Qxo.png",
+      "id": 2,
+      "name": "Tori Malcolm",
+    },
+  },
+}
+`;
 
-import state, { addInterview } from "helpers/__fixtures__/state";
-
-describe("selectors", () => {
-  describe("getTotalInterviews", () => {
-    it("should return 0 with no interviews scheduled", () => {
-      expect(getTotalInterviews(state)).toBe(0);
-    });
-
-    it("should return 1 with a single interview scheduled", () => {
-      const testState = addInterview(2, {
-        student: "Archie Cohen",
-        interviewer: 1
-      })(state);
-
-      expect(getTotalInterviews(testState)).toBe(1);
-    });
-  });
-
-  describe("getLeastPopularTimeSlot", () => {
-    it("should return a list when slots are equally unpopular", () => {
-      expect(getLeastPopularTimeSlot(state)).toBe("12pm, 1pm");
-    });
-
-    it("should return the least popular when there are more fewer interviews booked for a time slot", () => {
-      const testState = addInterview(2, {
-        student: "Archie Cohen",
-        interviewer: 1
-      })(state);
-
-      expect(getLeastPopularTimeSlot(testState)).toBe("12pm");
-    });
-  });
-
-  describe("getMostPopularDay", () => {
-    it("should return a list when there is a tie", () => {
-      expect(getMostPopularDay(state)).toBe("Monday, Tuesday");
-    });
-
-    it("should return Monday when it has the most appointments", () => {
-      const testState = addInterview(2, {
-        student: "Archie Cohen",
-        interviewer: 1
-      })(state);
-
-      expect(getMostPopularDay(testState)).toBe("Monday");
-    });
-  });
-
-  describe("getInterviewsPerDay", () => {
-    it("should return  when there are no interviews booked", () => {
-      expect(getInterviewsPerDay(state)).toBe(0);
-    });
-
-    it("should return 0.5 when there is one interview booked over two days", () => {
-      const testState = addInterview(2, {
-        student: "Archie Cohen",
-        interviewer: 1
-      })(state);
-
-      expect(getInterviewsPerDay(testState)).toBeCloseTo(0.5);
-    });
-
-    it("should return 1 when there are two interviews booked over two days", () => {
-      const testState = flow([
-        addInterview(2, {
-          student: "Archie Cohen",
-          interviewer: 1
-        }),
-        addInterview(3, {
-          student: "Lydia Miller-Jones",
-          interviewer: 2
-        })
-      ])(state);
-
-      expect(getInterviewsPerDay(testState)).toBe(1);
-    });
-  });
-});
+exports[`selectors setInterview should return state with one interview when interview is created and cancelled 1`] = `
+Object {
+  "appointments": Object {
+    "1": Object {
+      "id": 1,
+      "interview": null,
+      "time": "12pm",
+    },
+    "2": Object {
+      "id": 2,
+      "interview": Object {
+        "interviewer": 1,
+        "student": "Archie Cohen",
+      },
+      "time": "1pm",
+    },
+    "3": Object {
+      "id": 3,
+      "interview": null,
+      "time": "12pm",
+    },
+    "4": Object {
+      "id": 4,
+      "interview": null,
+      "time": "1pm",
+    },
+  },
+  "days": Array [
+    Object {
+      "appointments": Array [
+        1,
+        2,
+      ],
+      "id": 1,
+      "interviewers": Array [
+        1,
+        2,
+      ],
+      "name": "Monday",
+      "spots": 1,
+    },
+    Object {
+      "appointments": Array [
+        3,
+        4,
+      ],
+      "id": 2,
+      "interviewers": Array [
+        1,
+        2,
+      ],
+      "name": "Tuesday",
+      "spots": 2,
+    },
+  ],
+  "interviewers": Object {
+    "1": Object {
+      "avatar": "https://i.imgur.com/LpaY82x.png",
+      "id": 1,
+      "name": "Sylvia Palmer",
+    },
+    "2": Object {
+      "avatar": "https://i.imgur.com/Nmx0Qxo.png",
+      "id": 2,
+      "name": "Tori Malcolm",
+    },
+  },
+}
+`;
